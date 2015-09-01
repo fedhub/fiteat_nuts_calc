68495ded87a2c6c34bdf7fc7acde8ac1b598d4ef
shimonsw <shimi0007@hotmail.com>
2015-09-01 13:49:44 +0300

    first-commit

--- includes/js/javascript.js
+++ includes/js/javascript.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 
-    //var url = 'http://ndb.nal.usda.gov/ndb/foods/show/9?fg=&man=&lfacet=&count=&max=35&sort=&qlookup=01009&offset=&format=Full&new=&measureby=';
-    //$.ajax({
-    //    url: url,
-    //    crossOrigin: true,
-    //    method: 'GET',
-    //    success: function(res){
-    //        console.log(res);
-    //    },
-    //    fail: function(res){
-    //        console.log(res);
-    //    }
-    //});
+    var url = 'http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=Y0CJbZQ6ftSRQEfHMaYGIrdbWUZhidWF5Ehag3Yy&nutrients=208&ndbno=01009';
+    $.ajax({
+        url: url,
+        crossOrigin: true,
+        method: 'GET',
+        success: function(res){
+            console.log(res);
+        },
+        fail: function(res){
+            console.log(res);
+        }
+    });
 
-    // Using XMLHttpRequest
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", 'http://ndb.nal.usda.gov/ndb/foods/show/9?fg=&man=&lfacet=&count=&max=35&sort=&qlookup=01009&offset=&format=Full&new=&measureby=', true);
-    xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
-    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-    xhr.setRequestHeader("Access-Control-Allow-Headers", "X-Requested-With");
-    xhr.setRequestHeader("Access-Control-Allow-Headers", "Content-Type");
-    xhr.setRequestHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    xhr.onload = function () {
-        console.log(xhr.responseText);
-    };
-    xhr.send();
+    //// Using XMLHttpRequest
+    //var xhr = new XMLHttpRequest();
+    //xhr.open("GET", 'http://www.ndb.nal.usda.gov/ndb/foods/show/9?fg=&man=&lfacet=&count=&max=35&sort=&qlookup=01009&offset=&format=Full&new=&measureby=', true);
+    //xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
+    //xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    //xhr.setRequestHeader("Access-Control-Allow-Headers", "X-Requested-With");
+    //xhr.setRequestHeader("Access-Control-Allow-Headers", "Content-Type");
+    //xhr.setRequestHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    //xhr.onload = function () {
+    //    console.log(xhr.responseText);
+    //};
+    //xhr.send();
 
     //var data = "";
     //url = "http://www.example.com/page.html"
