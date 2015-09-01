7b5abe10cb21beea6c31ccb735aa6356c5db5f97
shimonsw <shimi0007@hotmail.com>
2015-09-01 12:56:45 +0300

    first-commit

--- includes/js/javascript.js
+++ includes/js/javascript.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
 
-    //var url = 'http://ndb.nal.usda.gov/ndb/foods/show/9?fg=&man=&lfacet=&count=&max=35&sort=&qlookup=01009&offset=&format=Full&new=&measureby=';
-    //$.ajax({
-    //    url: url,
-    //    method: 'GET',
-    //    success: function(res){
-    //        console.log(res);
-    //    },
-    //    fail: function(res){
-    //        console.log(res);
-    //    }
-    //});
+    var url = 'http://ndb.nal.usda.gov/ndb/foods/show/9?fg=&man=&lfacet=&count=&max=35&sort=&qlookup=01009&offset=&format=Full&new=&measureby=';
+    $.ajax({
+        url: url,
+        method: 'POST',
+        success: function(res){
+            console.log(res);
+        },
+        fail: function(res){
+            console.log(res);
+        }
+    });
 
-    // Using XMLHttpRequest
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", 'http://ndb.nal.usda.gov/ndb/foods/show/9?fg=&man=&lfacet=&count=&max=35&sort=&qlookup=01009&offset=&format=Full&new=&measureby=', true);
-    xhr.setRequestHeader("http://fiteat-nuts.herokuapp.com/", "some value");
-    xhr.onload = function () {
-        console.log(xhr.responseText);
-    };
-    xhr.send();
+    //// Using XMLHttpRequest
+    //var xhr = new XMLHttpRequest();
+    //xhr.open("GET", 'http://ndb.nal.usda.gov/ndb/foods/show/9?fg=&man=&lfacet=&count=&max=35&sort=&qlookup=01009&offset=&format=Full&new=&measureby=', true);
+    //xhr.setRequestHeader("http://fiteat-nuts.herokuapp.com/", "some value");
+    //xhr.onload = function () {
+    //    console.log(xhr.responseText);
+    //};
+    //xhr.send();
 });
